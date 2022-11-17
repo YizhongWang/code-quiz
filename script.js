c53f2e77cbeb1b3f9f91ce4c9c2e37c58ed2cadf
Yizhong Wang <wang.6689@osu.edu>
2022-11-17 18:31:15 -0500

    declare global variable

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions =[  
+    { 
+      question: 'What is not a JS primitive data type?',
+      answers: ["String", "Boolean", "Array", "Number"],
+      correctAnswer: "Array",
+
+    },
+    { 
+        question: 'What is not a JS primitive data type?',
+        answers: ["String", "Boolean", "Array", "Number"],
+        correctAnswer: "Array",
+  
+      },
+      { 
+        question: 'What is not a JS primitive data type?',
+        answers: ["String", "Boolean", "Array", "Number"],
+        correctAnswer: "rray",
+  
+      }
+
+]
+var startButton = document.getElementById("Start");
+var questionElement = document.getElementById("questions");
+var buttonA=document.getElementById("answer-A");
+var buttonB=document.getElementById("answer-B");
+var buttonC=document.getElementById("answer-C");
+var buttonD=document.getElementById("answer-D");
